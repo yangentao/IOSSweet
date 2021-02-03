3e234d04b9dd331840191efe0da385b596159839
Entao <entaoyang@163.com>
2021-02-03 16:22:59 +0800

    grid

--- Example/YetIOS/ui/layouts/Grid.swift
+++ Example/YetIOS/ui/layouts/Grid.swift
@@ -6,29 +6,121 @@
 import Foundation
 import UIKit
 
-public class GridParam {
-    public var width: CGFloat = MatchParent
-    public var height: CGFloat = MatchParent
-    public var gravityX: GravityX = .center
-    public var gravityY: GravityY = .center
+
+public extension UIView {
+    var gridParams: GridParams? {
+        get {
+            return getAttr("__gridParam__") as? GridParams
+        }
+        set {
+            setAttr("__gridParam__", newValue)
+        }
+    }
+
+    var gridParamsEnsure: GridParams {
+        if let L = self.gridParams {
+            return L
+        } else {
+            let a = GridParams()
+            self.gridParams = a
+            return a
+        }
+    }
+
+    @discardableResult
+    func gridParams(_ block: (GridParams) -> Void) -> Self {
+        block(gridParamsEnsure)
+        return self
+    }
+}
+
+public class GridParams {
+    public var width: CGFloat = 60
+    public var height: CGFloat = 60
+    public var gravityX: GravityX = .none
+    public var gravityY: GravityY = .none
     public var spanX: Int = 0
     public var spanY: Int = 0
+}
 
+public extension GridParams {
+    func width(_ w: CGFloat) -> Self {
+        self.width = w
+        return self
+    }
+
+    func height(_ h: CGFloat) -> Self {
+        self.height = h
+        return self
+    }
 }
 
 public class Grid: UIView {
-    public var axis: LayoutAxis = .vertical
+    public var axis: LayoutAxis = .vertical {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    public var gravityX: GravityX = .fill {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    public var gravityY: GravityY = .fill {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    public var columns: Int = 3 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
 
-    public var columns: Int = 3
+    public var hSpace: CGFloat = 0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    public var vSpace: CGFloat = 0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
 
-    public var hSpace: CGFloat = 0
-    public var vSpace: CGFloat = 0
+    private var contentSize: CGSize = .zero {
+        didSet {
+            if oldValue != contentSize {
+                processScroll()
+            }
+        }
+    }
 
-    public var gravityX: GravityX = .center
-    public var gravityY: GravityY = .center
+    public override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        processScroll()
+    }
 
+    private func processScroll() {
+        if let pv = self.superview as? UIScrollView {
+            pv.contentSize = contentSize
+        }
+    }
 
     public override func layoutSubviews() {
+        let childViews = self.subviews
+        if childViews.isEmpty {
+            return
+        }
+        if axis == .vertical {
+            calcSizeVertical(childViews)
+        } else {
+
+        }
+
+    }
+
+    private func calcSizeVertical(_ viewList: [UIView]) {
 
     }
 }
